5852009435c5f1c74d47cb415f6e0651a2ff7f8a
JacksonCrawford <57842293+JacksonCrawford@users.noreply.github.com>
2020-12-17 12:00:02 -0500

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,71 @@
+import foundation
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+import random
+
+# Uses requests to navigate to the Wired sitemap and grab all data under the specified class
+page = requests.get("https://www.wired.com/sitemap/")
+soupLinks = BeautifulSoup(page.text, "lxml")
+links = soupLinks.find(class_="sitemap__section-archive")
+
+# Finds all links with "1998" in them
+def masterLinker():
+    resume = False
+    linkList = list()
+    blacklist = ["https://www.wired.com/sitemap?year=1998&month=12&week=5", "https://www.wired.com/sitemap?year=1998&month=12&week=4", "https://www.wired.com/sitemap?year=1998&month=12&week=3", "https://www.wired.com/sitemap?year=1998&month=12&week=2", "https://www.wired.com/sitemap?year=1998&month=12&week=1", "https://www.wired.com/sitemap?year=1998&month=11&week=4", "https://www.wired.com/sitemap?year=1998&month=11&week=3", "https://www.wired.com/sitemap?year=1998&month=11&week=2"]
+    aTags = links.find_all("a")
+    for link in aTags:
+        url = link.contents[0]
+        if str(url) == "https://www.wired.com/sitemap?year=1998&month=5&week=2":
+            print("Banana")
+            resume = True
+        if "1998" in str(url) and resume:
+            linkList.append(url)
+    return linkList
+
+# Finds all links within the class of the site specified above
+def linker(url):
+    linkList = list()
+    try:
+        site = requests.get(url)
+    except:
+        t = time.localtime()
+        currentTime = time.strftime("%H:%M:%S", t)
+        print("Sleeping for 1 hour (3600 sec), beginning at:", currentTime)
+        time.sleep(3600)
+        site = requests.get(url)
+    
+    soupy = BeautifulSoup(site.text, "lxml")                                                        
+    linkz = soupy.find(class_="sitemap__section-archive") 
+    
+    aTags = linkz.find_all("a")
+    for link in aTags:
+        url = link.contents[0]
+        linkList.append(url)
+    return linkList
+
+# Main function that is run, uses a loop to evaluate data with scraper() 
+# in all links returned by linker()
+def main():
+    bigLinks = masterLinker()
+    linkNum = 4427
+    for bigLink in bigLinks:
+        print(str(bigLink) + "----------------") 
+
+        lilLinks = linker(bigLink)
+        for link in lilLinks:
+            data = foundation.scraper(link)
+            
+            timestamp = data["timestamp"]
+            fileDate = timestamp[:timestamp.find(" "):]
+            articleNum = f"{linkNum:04}"
+            
+            with open("wired_article_" + fileDate + "_" + articleNum + ".json", "w") as j:
+                json.dump(data, j)
+            print(str(link) + " -- #" + str(articleNum))
+            linkNum += 1
+            
+            time.sleep(random.randint(20, 55))
+main()
